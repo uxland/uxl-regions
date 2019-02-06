6c82c802ff773dd311cf67154821ef928a4d7463
dpinart <dpinart@newuibcn.com>
2019-02-06 09:28:36 +0100

    fix: upgrade dependencies and building

--- src/region-view.ts
+++ src/region-view.ts
@@ -1,22 +1,28 @@
-import {LitElement} from "lit-element";
+import {Constructor, LitElement} from "lit-element";
 import {dedupingMixin} from '@polymer/polymer/lib/utils/mixin';
 import {property} from "lit-element/lib/decorators";
 import {propertiesObserver} from '@uxland/uxl-utilities/properties-observer';
+import {MixinFunction} from "@uxland/uxl-utilities/types";
 export interface IRegionView {
     active: boolean;
     activeChanged(current: boolean, previous: boolean);
 
 }
+export interface RegionViewConstructor extends LitElement{
+    new(...args: any[]): IRegionView & LitElement;
+}
 export interface IRegionViewMixin<T = any> extends IRegionView, LitElement{
     new(): IRegionViewMixin<T> & T & LitElement;
 }
+export type RegionViewFunction = MixinFunction<RegionViewConstructor>;
 
-export const RegionView: <T>(parent: any) => IRegionViewMixin<T> = dedupingMixin( p =>{
-    class mixin extends propertiesObserver(p){
-        @property()
+export const regionView: RegionViewFunction = dedupingMixin((superClass: Constructor<LitElement>) =>{
+    class RegionView extends propertiesObserver(superClass) implements IRegionView{
+        @property({type: Boolean})
         active: boolean;
         activeChanged(current: boolean, previous: boolean){}
     }
-    return (<any> mixin) as IRegionViewMixin;
-});
+    return <any>RegionView;
+} );
+
 
