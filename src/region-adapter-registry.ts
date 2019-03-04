72ad4c4038fc749013b17a19e00cd597832a168b
dpinart <dpinart@newuibcn.com>
2019-03-04 14:26:59 +0100

    fix: adapter factory extended

--- src/region-adapter-registry.ts
+++ src/region-adapter-registry.ts
@@ -1,10 +1,6 @@
 import {IRegionAdapter, IRegionHost, RegionDefinition} from "./region";
 
-//export type adapterFactory = (definition: RegionDefinition, target: Element & IRegionHost) => IRegionAdapter;
-export interface adapterFactory {
-    (definition: RegionDefinition, target: Element & IRegionHost): IRegionAdapter;
-    test?: (target: Element) => boolean;
-}
+export type adapterFactory = (definition: RegionDefinition, target: Element & IRegionHost) => IRegionAdapter;
 const defaultAdapterKey = 'default';
 
 export class RegionAdapterRegistry {
@@ -13,28 +9,17 @@ export class RegionAdapterRegistry {
     registerAdapterFactory(key: any, adapter: adapterFactory) {
         this.adapterRegistry.set(key, adapter);
     }
-    private getCustomAdapterFactory = (host: Element) =>{
-        for (let entry of this.adapterRegistry){
-            if(typeof entry[0] === 'string' && entry[0] !== defaultAdapterKey && entry[1].test && entry[1].test(host)){
-                return entry[1]
-            }
-        }
-    }
-    private getFactory(host: Element): adapterFactory{
-         if (this.adapterRegistry.has(host.constructor))
-             return this.adapterRegistry.get(host.constructor);
-         if (this.adapterRegistry.has(host.localName))
-             return this.adapterRegistry.get(host.localName);
-         if (this.adapterRegistry.has(host.tagName))
-             return this.adapterRegistry.get(host.tagName);
-         if (this.adapterRegistry.has(defaultAdapterKey))
-             return this.adapterRegistry.get(defaultAdapterKey);
-         return null;
-    }
 
     getAdapterFactory(host: Element): adapterFactory {
-        let res = this.getCustomAdapterFactory(host);
-        return res || this.getFactory(host);
+        if (this.adapterRegistry.has(host.constructor))
+            return this.adapterRegistry.get(host.constructor);
+        if (this.adapterRegistry.has(host.localName))
+            return this.adapterRegistry.get(host.localName);
+        if (this.adapterRegistry.has(host.tagName))
+            return this.adapterRegistry.get(host.tagName);
+        if (this.adapterRegistry.has(defaultAdapterKey))
+            return this.adapterRegistry.get(defaultAdapterKey);
+        return null;
     }
 
     registerDefaultAdapterFactory(factory: adapterFactory) {
