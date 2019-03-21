1fb35ba866e32b504df238db35d4d7cce1b6a1e2
dpinart <dpinart@uxland.cat>
2019-03-21 03:05:01 +0100

    perf: new building pattern BREAKING CHANGE: esm built has been moved to es folder

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,10 @@ module.exports = {
   transform:{
     "^.+\\\\node_modules\\\\lit-element\\\\.*?\\\\*?\.js$": "<rootDir>/node_modules/ts-jest/preprocessor.js",
     "^.+\\node_modules\\lit-element\\.*?\\*?\.js$": "<rootDir>/node_modules/ts-jest/preprocessor.js",
+    "^.+\/node_modules\/lit-element\/.*?\/*?\.js$": "<rootDir>/node_modules/ts-jest/preprocessor.js",
     "^.+\\\\node_modules\\\\lit-html\\\\.*?\\\\*?\.js$": "<rootDir>/node_modules/ts-jest/preprocessor.js",
     "^.+\\node_modules\\lit-html\\.*?\\*?\.js$": "<rootDir>/node_modules/ts-jest/preprocessor.js",
+    "^.+\/node_modules\/lit-html\/.*?\/*?\.js$" : "<rootDir>/node_modules/ts-jest/preprocessor.js",
     "^.+\\.ts$": "<rootDir>/node_modules/ts-jest/preprocessor.js",
     "^.+\.ts$": "<rootDir>/node_modules/ts-jest/preprocessor.js"
   }
