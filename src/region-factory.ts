27d54ba7b24750a48e23be8e778ce4688d3cf668
danielcabiscol <danielcabiscol@gmail.com>
2019-09-02 17:48:09 +0200

    fix: await updatePromise and microTask created region

--- src/region-factory.ts
+++ src/region-factory.ts
@@ -1,19 +1,25 @@
-import {Region, RegionDefinition} from "./region";
-import {IRegionManager} from "./region-manager";
-import {RegionAdapterRegistry} from "./region-adapter-registry";
-import {invariant} from '@uxland/uxl-utilities';
+import { Region, RegionDefinition } from './region';
+import { IRegionManager } from './region-manager';
+import { RegionAdapterRegistry } from './region-adapter-registry';
+import { invariant } from '@uxland/uxl-utilities';
 
-const createAdapter = (definition: RegionDefinition, target: Element,adapterRegistry: RegionAdapterRegistry) =>{
-    let adapterFactory = adapterRegistry.getAdapterFactory(target);
-    invariant(typeof adapterFactory === 'function', 'No region adapter factory found for the host');
-    return adapterFactory(definition, <any>target);
+const createAdapter = (definition: RegionDefinition, target: Element, adapterRegistry: RegionAdapterRegistry) => {
+  let adapterFactory = adapterRegistry.getAdapterFactory(target);
+  invariant(typeof adapterFactory === 'function', 'No region adapter factory found for the host');
+  return adapterFactory(definition, <any>target);
 };
-export const regionFactory = (definition: RegionDefinition, host: Element, regionManager: IRegionManager, adapterRegistry: RegionAdapterRegistry) =>{
-    let target = host.shadowRoot.querySelector(`#${definition.targetId}`);
-    let adapter = definition.adapterFactory ? definition.adapterFactory(definition, <any>target) : createAdapter(definition, target, adapterRegistry);
-    invariant(adapter, 'No region adapter found for the host');
-    let targetRegionManager = definition.scoped ? regionManager.createRegionManager() : regionManager;
-    let region = new Region(definition.name, targetRegionManager, target as any, adapter, definition);
-    targetRegionManager.add(definition.name, region);
-    return region;
+export const regionFactory = async (
+  definition: RegionDefinition,
+  host: Element,
+  regionManager: IRegionManager,
+  adapterRegistry: RegionAdapterRegistry
+) => {
+  await (host as any)._updatePromise;
+  let target = host.shadowRoot.querySelector(`#${definition.targetId}`);
+  let adapter = definition.adapterFactory ? definition.adapterFactory(definition, <any>target) : createAdapter(definition, target, adapterRegistry);
+  invariant(adapter, 'No region adapter found for the host');
+  let targetRegionManager = definition.scoped ? regionManager.createRegionManager() : regionManager;
+  let region = new Region(definition.name, targetRegionManager, target as any, adapter, definition);
+  targetRegionManager.add(definition.name, region);
+  return region;
 };
--- src/region-host-mixin.ts
+++ src/region-host-mixin.ts
@@ -1,58 +1,60 @@
-import {IRegion, RegionDefinition} from "./region";
-import {regionsProperty} from "./region-decorator";
-import {IRegionManager, regionManager} from "./region-manager";
-import {regionFactory} from "./region-factory";
-import {regionAdapterRegistry, RegionAdapterRegistry} from "./region-adapter-registry";
-import {factory} from "./adapters/multiple-active-adapter";
-import {Constructor, LitElement, PropertyValues} from "lit-element";
-import {MixinFunction, dedupingMixin} from "@uxland/uxl-utilities";
+import { IRegion, RegionDefinition } from './region';
+import { regionsProperty } from './region-decorator';
+import { IRegionManager, regionManager } from './region-manager';
+import { regionFactory } from './region-factory';
+import { regionAdapterRegistry, RegionAdapterRegistry } from './region-adapter-registry';
+import { factory } from './adapters/multiple-active-adapter';
+import { Constructor, LitElement, PropertyValues } from 'lit-element';
+import { MixinFunction, dedupingMixin, microTask } from '@uxland/uxl-utilities';
 
-export interface IRegionHostMixin<T = any> extends LitElement{
-    new() : IRegionHostMixin<T> & T & LitElement;
+export interface IRegionHostMixin<T = any> extends LitElement {
+  new (): IRegionHostMixin<T> & T & LitElement;
 }
-export interface RegionHostMixin extends LitElement{
-
+export interface RegionHostMixin extends LitElement {
+  regionsCreated(): void;
 }
-export interface RegionHostMixinConstructor extends LitElement{
-    new(...args: any[]): RegionHostMixin & LitElement;
+export interface RegionHostMixinConstructor extends LitElement {
+  new (...args: any[]): RegionHostMixin & LitElement;
 }
 export type RegionHostMixinFunction = MixinFunction<RegionHostMixinConstructor>;
 
-const getUxlRegions: (item: any) => {[key: string]: RegionDefinition} = item => item.constructor[regionsProperty] || {};
-export const RegionHostMixin: (regionManager: IRegionManager, adapterRegistry: RegionAdapterRegistry) => RegionHostMixinFunction = (regionManager1, adapterRegistry) => dedupingMixin(
-    ((superClass: Constructor<LitElement>) =>{
-        class RegionHostMixinClass extends superClass implements RegionHostMixin{
-            protected firstUpdated(changedProperties: PropertyValues): void {
-                super.firstUpdated(changedProperties);
-                let regions = getUxlRegions(this);
-                Object.keys(regions).forEach(name =>{
-                    let region = regionFactory(regions[name], <any>this, regionManager, adapterRegistry);
-                    this[name] = region;
-                    let behaviors = region.adapter ? region.adapter.behaviors || [] : [];
-                    behaviors.forEach(b => b.attach());
-                });
-
-            }
-            disconnectedCallback(): void {
-                super.disconnectedCallback();
-                let regions = getUxlRegions(this);
-                Object.keys(regions).forEach(name =>{
-                    let region = this[name] as IRegion;
-                    if(region){
-                        region.regionManager.remove(region);
-                        let behaviors = region.adapter ? region.adapter.behaviors || [] : [];
-                        behaviors.forEach(b => b.detach());
-                    }
-
-                });
-            }
-        }
-        return <any>RegionHostMixinClass;
-    } )
-);
+const getUxlRegions: (item: any) => { [key: string]: RegionDefinition } = item => item.constructor[regionsProperty] || {};
+export const RegionHostMixin: (regionManager: IRegionManager, adapterRegistry: RegionAdapterRegistry) => RegionHostMixinFunction = (
+  regionManager1,
+  adapterRegistry
+) =>
+  dedupingMixin((superClass: Constructor<LitElement>) => {
+    class RegionHostMixinClass extends superClass implements RegionHostMixin {
+      protected firstUpdated(changedProperties: PropertyValues): void {
+        super.firstUpdated(changedProperties);
+        let regions = getUxlRegions(this);
+        microTask.run(async () => {
+          for (let name in regions) {
+            let region = await regionFactory(regions[name], this, regionManager, adapterRegistry);
+            this[name] = region;
+            let behaviors = region.adapter ? region.adapter.behaviors || [] : [];
+            behaviors.forEach(b => b.attach());
+          }
+          this.regionsCreated();
+        });
+      }
+      regionsCreated() {}
+      disconnectedCallback(): void {
+        super.disconnectedCallback();
+        let regions = getUxlRegions(this);
+        Object.keys(regions).forEach(name => {
+          let region = this[name] as IRegion;
+          if (region) {
+            region.regionManager.remove(region);
+            let behaviors = region.adapter ? region.adapter.behaviors || [] : [];
+            behaviors.forEach(b => b.detach());
+          }
+        });
+      }
+    }
+    return <any>RegionHostMixinClass;
+  });
 
 regionAdapterRegistry.registerDefaultAdapterFactory(factory);
 
 export const RegionHost: RegionHostMixinFunction = RegionHostMixin(regionManager, regionAdapterRegistry);
-
-
