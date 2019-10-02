e4d357a284d58c507b80a240057482a6a6771d8b
Alex Vizcaino <avizcaino@uxland.es>
2019-10-02 09:46:09 +0200

    fix: add route to view definition

--- src/view-definition.ts
+++ src/view-definition.ts
@@ -1,19 +1,20 @@
-import {IRegion} from "./region";
+import { IRegion } from './region';
 
 export type ViewFactory = () => Promise<HTMLElement>;
 export interface ViewDefinition {
-    htmlTag?: string;
-    htmlUrl?: string;
-    factory?: ViewFactory;
-    element?: HTMLElement;
-    options?: any;
-    isDefault?: boolean;
-    removeFromDomWhenDeactivated?: boolean;
-    sortHint?: string;
+  htmlTag?: string;
+  htmlUrl?: string;
+  factory?: ViewFactory;
+  element?: HTMLElement;
+  options?: any;
+  isDefault?: boolean;
+  removeFromDomWhenDeactivated?: boolean;
+  sortHint?: string;
+  route?: string;
 }
 export interface ViewComponent {
-    view: ViewDefinition;
-    viewKey: string;
-    region: IRegion;
-    active: boolean;
+  view: ViewDefinition;
+  viewKey: string;
+  region: IRegion;
+  active: boolean;
 }
