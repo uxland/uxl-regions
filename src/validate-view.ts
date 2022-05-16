39fdb94918216956983932276171ab5f4647a627
Alex Vizcaino <avizcaino@uxland.es>
2022-05-16 14:20:11 +0200

    fix: solves issue with initial instance of class when declared by factory

--- src/validate-view.ts
+++ src/validate-view.ts
@@ -1,16 +1,34 @@
-import {ViewDefinition} from "./view-definition";
-import {invariant} from '@uxland/uxl-utilities';
-const isDomElement = (element: HTMLElement) => typeof HTMLElement === 'object' ?
-    element instanceof HTMLElement :
-    element && typeof element === 'object' && element != null && element.nodeType === 1 && typeof element.nodeName === 'string';
+import { invariant } from "@uxland/uxl-utilities";
+import { ViewDefinition } from "./view-definition";
+const isDomElement = (element: HTMLElement) =>
+  typeof HTMLElement === "object"
+    ? element instanceof HTMLElement
+    : element &&
+      typeof element === "object" &&
+      element != null &&
+      element.nodeType === 1 &&
+      typeof element.nodeName === "string";
 
-export const validateView = (view: ViewDefinition) =>{
-    invariant(view.htmlTag || view.element || view.factory, 'One of properties htmlTag, factory or element must be set');
-    if(view.htmlTag)
-        invariant(typeof view.htmlTag === 'string', 'htmlTag property must be an string');
-    if(view.factory)
-        invariant(typeof view.factory === 'function', 'factory property must be a function');
-    if(view.element)
-        invariant(isDomElement(view.element), 'element property must be an HTMLElement');
-    return true;
+export const validateView = (view: ViewDefinition) => {
+  if (!view.htmlTag && !view.element && !view.factory)
+    throw new Error(
+      "One of properties htmlTag, factory or element must be set"
+    );
+  //invariant(view.htmlTag || view.element || view.factory, 'One of properties htmlTag, factory or element must be set');
+  if (view.htmlTag)
+    invariant(
+      typeof view.htmlTag === "string",
+      "htmlTag property must be an string"
+    );
+  if (view.factory)
+    invariant(
+      typeof view.factory === "function",
+      "factory property must be a function"
+    );
+  if (view.element)
+    invariant(
+      isDomElement(view.element),
+      "element property must be an HTMLElement"
+    );
+  return true;
 };
